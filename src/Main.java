86b953ceda53adb648c13911c85964a7cb151804
barrosjd <barrosjd2@gmail.com>
2023-04-13 21:44:34 -0300

    criação das classes Mentoria, Curso e Conteudo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Curso curso1 = new Curso();
+
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição curso Jasva");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+
+        curso2.setTitulo("Curso JS");
+        curso2.setDescricao("Descrição curso JS");
+        curso2.setCargaHoraria(4);
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+
+        Mentoria mentoria = new Mentoria();
+
+        mentoria.setTitulo("Mentoria Java");
+        mentoria.setDescricao("Descrição mentoria Jasva");
+        mentoria.setData(LocalDate.now());
+
+
     }
 }
